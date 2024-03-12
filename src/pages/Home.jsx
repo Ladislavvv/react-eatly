cd9c9082c9ef6965a6a51ed1d5724062ef5d462c
Vladislav <zhukovdev@mail.ru>
2024-03-13 02:00:13 +0300

    Home page - mobile-app sectiong

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -47,7 +47,7 @@ function Home() {
         </div>
       </section>
       <section className="statistics__section">
-        <div className='bg'></div>
+        <div className="bg"></div>
         <ul className="statistics__section_list">
           <li className="statistics__section_list-item">
             <ul>
@@ -77,25 +77,24 @@ function Home() {
         </ul>
       </section>
       <section className="mobile-app__section">
-        <img className="mobile-app__section-img" src="../assets/mobile.png" alt="phone app" />
+        <img className="mobile-app__section-img" src="/img/mobile.png" alt="phone app" />
         <div className="mobile-app__section_sideText">
           <h2 className="sideText__title">
             Premium <span>Quality</span> <br /> For Your Health
           </h2>
           <ul className="sideText__list">
             <li className="sideText__list-item">
-              Premium quality food is made with ingredients that are packed with essential vitamins,
+              Premium quality food is made with ingredients that <br /> are packed with essential vitamins,
               minerals.
             </li>
             <li className="sideText__list-item">
-              These foods promote overall wellness by support healthy digestion and boosting
+              These foods promote overall wellness by support <br /> healthy digestion and boosting
               immunity
             </li>
-            <button className="sideText__btn">Download</button>
           </ul>
+          <button className="sideText__btn">Download</button>
         </div>
       </section>
-      c
     </>
   );
 }
